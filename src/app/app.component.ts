2a5d5d11e47e093c1cd3cfd6c825f2362602578a
LucasSrSilva <lucas.srs.2022@gmail.com>
2024-05-29 10:15:41 -0300

    curso 2 Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { FormularioContatoComponent } from './paginas/formulario-contato/formulario-contato.component';
+import { ListaContatosComponent } from './paginas/lista-contatos/lista-contatos.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    FormularioContatoComponent,
+    ListaContatosComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'indexa';
+  
 }
---
+++ src/app/paginas/lista-contatos/lista-contatos.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+import { ContainerComponent } from '../../componentes/container/container.component';
+import { CabecalhoComponent } from '../../componentes/cabecalho/cabecalho.component';
+import { SeparadorComponent } from '../../componentes/separador/separador.component';
+import { ContatoComponent } from '../../componentes/contato/contato.component';
+import { ContatoService } from '../../services/contato.service';
+import { FormularioContatoComponent } from '../formulario-contato/formulario-contato.component';
+import { Contato } from '../../componentes/contato/contato';
+
+
+@Component({
+  selector: 'app-lista-contatos',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ContainerComponent,
+    CabecalhoComponent,
+    SeparadorComponent,
+    ContatoComponent,
+    FormsModule,
+    FormularioContatoComponent,
+    RouterLink
+  ],
+  templateUrl: './lista-contatos.component.html',
+  styleUrl: './lista-contatos.component.css'
+})
+export class ListaContatosComponent implements OnInit{
+  alfabeto: string = "abcdefghijklmnopqrstuvwxyz";
+  contatos: Contato[] = [];
+
+  filtroPorTexto: string = "";
+
+  constructor(private contatoService: ContatoService){}
+
+  ngOnInit(){
+    this.contatos = this.contatoService.obterContatos();
+  }
+
+  temContatosParaLetra(letra: string): boolean {
+    if (this.contatos) {
+      return this.contatos.filter(contato => contato.nome.toLowerCase().startsWith(letra.toLowerCase())).length > 0;
+    }
+    return false;
+  }
+
+  removerAcentos(texto: string): string{
+    return texto.normalize("NFD").replace(/[\u0300-\u036f]/g, "")
+  }
+
+  filtrarContatosPorTexto(): Contato[]{
+    if (!this.filtroPorTexto){
+      return this.contatos
+    }
+    return this.contatos.filter(contato => {
+      return this.removerAcentos(contato.nome).toLowerCase().includes(this.removerAcentos(this.filtroPorTexto).toLowerCase())
+    })
+  }
+
+  filtrarContatosOrdemAlfabetica(letra: string): Contato[]{
+    if (this.contatos) {
+      return this.filtrarContatosPorTexto().filter(contato => {
+        return this.removerAcentos(contato.nome).toLowerCase().startsWith(this.removerAcentos(letra));
+      });
+    }
+    return [];
+  }
+}
